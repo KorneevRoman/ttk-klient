43e31a33220fbe862c1fc5d2914901824e8dd4db
nikolai_job <ursegovnikolaj@gmail.com>
2023-03-19 16:21:15 +0300

    next project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import {NavBar} from "@/features/layout/NavBar";
 export default function App({Component, pageProps}: AppProps) {
     return (
         <NavBar>
+
             <Component {...pageProps} />
         </NavBar>
     )
